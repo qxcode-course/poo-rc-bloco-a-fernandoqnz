c8fe9a7429e2700a268f8d5ade7b93863caf2fb2
Fernando Castro <fernand4603b@gmail.com>
2025-09-25 20:45:28 +0000

    tentando desenrolar atividade do animal

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -1,11 +1,28 @@
 class Animal():
-    def __init__(self, species: str, sound: str, age:int):
+    def __init__(self, species: str, sound: str):
         self.species:str=species
         self.sound:str=sound
         self.age:int=0
 
 
-    def ageBy(self, amount:int):
-        self.age+=4 
+    def __str__(self) -> str:
+        return (f"{self.species}:{self.age}:{self.sound} morreu.")
+    
+    def ageBy(self, increment:int):
+        if self.age==4:
+            return(f"{Warning:self.species} morreu.")
+        
+        self.age += increment
+        if self.age >= 4:
+            self.age = 4
+            return(f"{Warning:self.species} morreu.")
+            
+    def makeSound(self):
+        if self.age==0:
+            return("---")
+        if self.age==4:
+            return("morreu.")
+        return self.sound
+
         
     
